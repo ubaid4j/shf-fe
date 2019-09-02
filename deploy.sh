7eb6080ec3c9158a2efbca851843712b51737dfe
UbaidurRehman1 <ubaidshan007@gmail.com>
2019-09-02 23:57:03 +0500

    prod

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 sudo git pull
 sudo npm install
-sudo ng build --base-href=/shf-fe/
+sudo ng build --prod --base-href=/shf-fe/
 sudo rm -rf /opt/tomcat/webapps/shf-fe
 sudo mv dist/shf-fe /opt/tomcat/webapps/

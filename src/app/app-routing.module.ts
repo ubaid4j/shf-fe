dc4d50f33fed732d31ef7ff6cdd8fdb1d8ad9da9
UbaidurRehman1 <ubaidshan007@gmail.com>
2019-09-15 18:16:35 +0500

    login form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 const routes: Routes = [
     {path: '', component : LandingPageComponent},
+    {path: 'login', component : LoginComponent},
     {path: '**', component : PageNotFoundComponent}
 ];
 
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -7,9 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+    private username : string = 'ubaid';
+    private password : string = 'test123';
+    private isInValid : boolean = false;
 
-  ngOnInit() {
-  }
+
+    constructor()
+    {
+
+    }
+
+    ngOnInit()
+    {
+
+    }
+
+    public whenClick() : void
+    {
+        // console.log("Before: isAuth: " + sessionStorage.getItem(HardCodeAuthService.AUTH));
+        // console.log(this.username);
+        // console.log(this.password);
+        console.log("Clicked");
+        //TEST
+        // this.giveAuth();
+
+        // this.jwtAuth.auth(this.username, this.password).subscribe(
+        //     data => {
+        //         console.log(data);
+        //         this.isInValid = false;
+        //         this.router.navigate(['welcome', this.username]);
+        //         console.log("After: isAuth: " + sessionStorage.getItem(AUTHUSER));
+        //     },
+        //     error => {
+        //         console.log(error);
+        //         this.isInValid = true;
+        //     }
+        // );
+    }
 
 }
